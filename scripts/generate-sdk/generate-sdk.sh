ee906cfe6fa7181c09d918bffbba543b95621b4b
Henrique Santos <henrique.santos@freiheit.com>
2023-09-19 16:18:01 +0100

    Fix typo

--- scripts/generate-sdk/generate-sdk.sh
+++ scripts/generate-sdk/generate-sdk.sh
@@ -13,7 +13,7 @@ SDK_REPO="https://github.com/stackitcloud/stackit-sdk-go.git"
 SDK_GO_VERSION="1.18"
 OAS_REPO=https://github.com/stackitcloud/stackit-api-specifications
 
-# renovate: datasource=github-tags depName=OpenAPITools/openapi-generator versioning=regex:^(v)?(?<version>[0-9]+\.[0-9]+\.[0-9]+)$
+# Renovate: datasource=github-tags depName=OpenAPITools/openapi-generator versioning=regex:^(v)?(?<version>[0-9]+\.[0-9]+\.[0-9]+)$
 GENERATOR_VERSION="v6.5.0"
 GENERATOR_VERSION_NUMBER="${GENERATOR_VERSION:1}"
 
