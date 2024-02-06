426718253c02f3b2e4a5c25593e7d0184dfd3998
Henrique Santos <henrique.santos@freiheit.com>
2024-02-06 17:31:39 +0000

    Fix go.mod check failing when module is generated for the first time

--- scripts/generate-sdk/generate-sdk.sh
+++ scripts/generate-sdk/generate-sdk.sh
@@ -132,12 +132,15 @@ for service_json in ${ROOT_DIR}/oas/*.json; do
     rm ${SDK_REPO_LOCAL_PATH}/services/${service}/.openapi-generator/FILES
 
     # If there's a comment at the start of go.mod, copy it
-    go_mod_first_line="$(head -n 1 ${sdk_services_backup_dir}/${service}/go.mod)"
-    is_comment_pattern="^\/\/"
-    if [[ ${go_mod_first_line} =~ ${is_comment_pattern} ]]; then
-        echo "Found comment at the top of ${service}/go.mod"
-        go_mod_path="${SDK_REPO_LOCAL_PATH}/services/${service}/go.mod"
-        echo -e "${go_mod_first_line}\n$(cat ${go_mod_path})" >${go_mod_path}
+    go_mod_backup_path="${sdk_services_backup_dir}/${service}/go.mod"
+    if [ -f ${go_mod_backup_path} ]; then
+        go_mod_backup_first_line="$(head -n 1 ${go_mod_backup_path})"
+        is_comment_pattern="^\/\/"
+        if [[ ${go_mod_backup_first_line} =~ ${is_comment_pattern} ]]; then
+            echo "Found comment at the top of ${service}/go.mod"
+            go_mod_path="${SDK_REPO_LOCAL_PATH}/services/${service}/go.mod"
+            echo -e "${go_mod_backup_first_line}\n$(cat ${go_mod_path})" >${go_mod_path}
+        fi
     fi
 
     # Move tests to the service folder
