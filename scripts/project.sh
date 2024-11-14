b67c0d0f2a1b8c02f33d38d0e7da70e19e7f4b43
Melvin <70433111+MelvinKl@users.noreply.github.com>
2024-11-14 13:50:22 +0100

    chore: adds language keyword for installation of tools (#110)

--- scripts/project.sh
+++ scripts/project.sh
@@ -11,13 +11,24 @@ SDK_PATH="${ROOT_DIR}/sdk"
 
 action=$1
 
+if [[ -z $2 ]]; then
+    echo "LANGUAGE not specified, default will be used."
+    LANGUAGE="go"
+else
+    LANGUAGE=$2
+fi
+
 if [ "$action" = "help" ]; then
     [ -f "$0".man ] && man "$0".man || echo "No help, please read the script in ${script}, we will add help later"
 elif [ "$action" = "tools" ]; then
     cd ${ROOT_DIR}
-
-    go install golang.org/x/tools/cmd/goimports@latest
-    pip install black==24.8.0 isort~=5.13.2 autoimport~=1.6.1
+    if [ "${LANGUAGE}" == "go" ]; then
+        go install golang.org/x/tools/cmd/goimports@latest
+    elif [ "${LANGUAGE}" == "python" ]; then
+        pip install black==24.8.0 isort~=5.13.2 autoimport~=1.6.1
+    else
+        echo "Invalid language: `$LANGUAGE`, please use $0 help for help"
+    fi
 else
     echo "Invalid action: '$action', please use $0 help for help"
 fi
