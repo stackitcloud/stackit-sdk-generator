95c2db2a591a5354112a21b4dea545b79170168e
João Palet <joao.palet@freiheit.com>
2024-11-27 17:35:53 +0000

    feat: Lint and test service SDK before pushing

--- scripts/sdk-create-pr.sh
+++ scripts/sdk-create-pr.sh
@@ -11,19 +11,19 @@ BRANCH_PREFIX=$1
 COMMIT_INFO=$2
 
 if [ $# -lt 2 ]; then
-    echo "Not enough arguments supplied. Required: 'branch-prefix' 'commit-info'"
+    echo "! Not enough arguments supplied. Required: 'branch-prefix' 'commit-info'"
     exit 1
 fi
 
 if type -p go >/dev/null; then
     :
 else
-    echo "Go not installed, unable to proceed."
+    echo "! Go not installed, unable to proceed."
     exit 1
 fi
 
 if [ ! -d ${SDK_REPO_LOCAL_PATH} ]; then
-    echo "sdk to commit not found in root. Please run make generate-sdk"
+    echo "! SDK to commit not found in root. Please run make generate-sdk"
     exit 1
 fi
 
@@ -41,7 +41,8 @@ else
 fi
 
 # Create temp directory to work on
-work_dir=$(mktemp -d)
+work_dir="$ROOT_DIR/temp"
+mkdir -p ${work_dir}
 if [[ ! ${work_dir} || -d {work_dir} ]]; then
     echo "Unable to create temporary directory"
     exit 1
@@ -51,7 +52,7 @@ fi
 trap "rm -rf ${work_dir}" EXIT
 
 mkdir ${work_dir}/git_repo    # Where the git repo will be created
-mkdir ${work_dir}/sdk_backup # Backup of the SDK to check for new modules
+mkdir ${work_dir}/sdk_backup  # Backup of the SDK to check for new modules
 mkdir ${work_dir}/sdk_to_push # Copy of SDK to push
 
 # Prepare SDK to push
@@ -60,7 +61,7 @@ rm -rf ${work_dir}/sdk_to_push/.git
 
 # Initialize git repo
 cd ${work_dir}/git_repo
-git clone ${REPO_URL_SSH} ./
+git clone ${REPO_URL_SSH} ./ --quiet
 git config user.name "${COMMIT_NAME}"
 git config user.email "${COMMIT_EMAIL}"
 
@@ -74,37 +75,50 @@ for service_path in ${work_dir}/sdk_to_push/services/*; do
     # Removal of pulled data is necessary because the old version may have files
     # that were deleted in the new version
     rm -rf ./services/$service/*
-    cp -a ${work_dir}/sdk_to_push/services/$service/. ./services/$service 
+    cp -a ${work_dir}/sdk_to_push/services/$service/. ./services/$service
 
     # Check for changes in the specific folder compared to main
     service_changes=$(git status --porcelain "services/$service")
 
     if [[ -n "$service_changes" ]]; then
-        echo "Committing changes for $service" 
+        echo -e "\n>> Detected changes in $service service"
+
+        # If lint or test fails for a service, we skip it and continue to the next one
+        make lint skip-non-generated-files=true service=$service || {
+            echo "! Linting failed for $service. THE UPDATE OF THIS SERVICE WILL BE SKIPPED."
+            continue
+        }
+        make test skip-non-generated-files=true service=$service || {
+            echo "! Testing failed for $service. THE UPDATE OF THIS SERVICE WILL BE SKIPPED."
+            continue
+        }
+
         if [[ "$BRANCH_PREFIX" != "main" ]]; then
             git switch main # This is needed to create a new branch for the service without including the previously committed files
             branch="$BRANCH_PREFIX/$service"
             git switch -c "$branch"
         else
             branch=$BRANCH_PREFIX
-        fi            
-        
+        fi
+
         git add services/${service}/
         if [ "${LANGUAGE}" == "go" ] && [ ! -d "${work_dir}/sdk_backup/services/${service}/" ]; then # Check if it is a newly added SDK module
             # go work use -r adds a use directive to the go.work file for dir, if it exists, and removes the use directory if the argument directory doesn’t exist
             # the -r flag examines subdirectories of dir recursively
             # this prevents errors if there is more than one new module in the SDK generation
-            go work use -r . 
+            go work use -r .
             git add go.work
         fi
-        
+
         if [[ "$branch" != "main" ]]; then
+            echo ">> Creating PR for $service"
             git commit -m "Generate $service"
             git push origin "$branch"
             gh pr create --title "Generator: Update SDK /services/$service" --body "$COMMIT_INFO" --head "$branch" --base "main"
         else
+            echo ">> Pushing changes for $service service..."
             git commit -m "Generate $service: $COMMIT_INFO"
             git push origin "$branch"
-        fi   
+        fi
     fi
 done
